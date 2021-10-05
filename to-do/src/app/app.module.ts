f8161a3503df6538bf9c6dec0b3c632b3d576c0f
owarner <owarner@ceiamerica.com>
2021-10-05 10:39:34 -0400

    added todo form

--- to-do/src/app/app.module.ts
+++ to-do/src/app/app.module.ts
@@ -3,24 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListViewComponent } from './list-view/list-view.component';
-import { CardViewComponent } from './card-view/card-view.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { MaterialModule } from './material/material.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListViewComponent,
-    CardViewComponent,
     HomeComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
